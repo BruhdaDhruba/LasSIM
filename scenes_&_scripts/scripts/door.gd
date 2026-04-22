6e8c9c7c2416bf7ffb91b125117cd1e6e2578426
Dhruba Das <brodydas@gmail.com>
2026-04-22 16:45:36 +0200

    Added door logic & scene transition

--- scenes_&_scripts/scripts/door.gd
+++ scenes_&_scripts/scripts/door.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
-@export var target_scene: String = ""
+@export_file("*.tscn") var target_scene: String = ""
 
 func interact():
 	if target_scene != "":
-		get_tree().change_scene_to_file(target_scene)
+		call_deferred("_change_scene")
+
+func _change_scene():
+	get_tree().change_scene_to_file(target_scene)
