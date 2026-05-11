3a48563b0b2677f4c8533a3675e286b47ad5b305
Dhruba Das <brodydas@gmail.com>
2026-05-11 23:01:42 +0200

    npc model & script updated

--- scenes_&_scripts/scripts/npc.gd
+++ scenes_&_scripts/scripts/npc.gd
@@ -1,11 +1,22 @@
 extends Node3D
 
-var dialogue_lines = [
+@export var npc_name = "NPC"
+@export var dialogue_lines = [
 	"Hej!",
 	"Kan vi få se leg?",
-    "Välkommen in!"
+	"Välkommen in!"
 ]
+
+@onready var name_label = $NameLabel
+
+func _ready():
+	name_label.text = npc_name
 
 func interact():
 	var dialogue_ui = get_tree().get_root().get_node("Main/DialogueUI")
-	dialogue_ui.start_dialogue(dialogue_lines)
+
+	if dialogue_ui == null:
+		dialogue_ui = get_tree().get_first_node_in_group("dialogue_ui")
+
+	if dialogue_ui:
+		dialogue_ui.start_dialogue(dialogue_lines)
